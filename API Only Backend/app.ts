e26175759f335e3cb065942ddebbe1454e620e51
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-02-16 19:22:50 +1300

    updated prisma imports

--- API Only Backend/app.ts
+++ API Only Backend/app.ts
@@ -1,1 +1,27 @@
-import
+import "dotenv/config";
+import express from "express";
+import { resolveSoa } from "node:dns";
+// import indexRouter from "./routes/indexRouter";
+// import "./controllers/indexController.ts";
+const app = express();
+import path from "node:path";
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Blog API Backend - listening on port ${PORT}!`);
+});
