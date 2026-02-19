a4da557fd8d12bf7d31e9cb24b2d7384d2c74021
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-02-19 17:15:00 +1300

    moved files to root and set up getblogposts call

---
+++ Blog Site Frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import './App.css'
+
+const App = () => {
+  const [blogPosts, setBlogPosts] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/blogPosts/view')
+      .then((response) => response.json())
+      .then((data) => {
+        setBlogPosts(data);
+        console.log(data);
+      })
+    .catch((error) => console.error(error))
+
+  }, []);
+
+  return (
+    <>
+      <Outlet context={{ blogPosts }} />
+    </>
+  )
+}
+
+export default App
