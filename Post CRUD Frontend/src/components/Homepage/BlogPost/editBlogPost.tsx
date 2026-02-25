d0054ca58b7a4f50727f2ca8bd8f60509c2b2d0a
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-02-26 00:28:01 +1300

    added tinymce, debugging wip

--- Post CRUD Frontend/src/components/Homepage/BlogPost/editBlogPost.tsx
+++ Post CRUD Frontend/src/components/Homepage/BlogPost/editBlogPost.tsx
@@ -3,12 +3,16 @@ import { Editor } from '@tinymce/tinymce-react';
 import type { blogPost } from "../../../types/blogPosts";
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
-
+import {Editor as TinyMCEEditor} from "tinymce"; 
 
 
 function EditBlogPostPage() {
+
+    
     const [post, setPost] = useState<blogPost | null>(null);
     const { postId } = useParams();
+    const [content, setContent] = useState<string>("");
+    const [title, setTitle] = useState<string>("");
     useEffect(() => {
         async function fetchPost() {
           const response = await fetch("http://localhost:3000/blogPosts/view/", {
@@ -21,38 +25,42 @@ function EditBlogPostPage() {
           const data = await response.json();
           console.log(data);
           setPost(data.blogPost);
+          setContent(data.blogPost.text);
+          setTitle(data.blogPost.title);
           return;
         }
-
         fetchPost();
     }, [postId])
-    const editorRef = useRef(null);
+    const editorRef = useRef<TinyMCEEditor | null>(null);
     const log = () => {
+        if (editorRef.current) {
+        console.log(editorRef.current.getContent());
+        }
     };
     return (
-        <>
-
-        <Editor
-            apiKey='your-api-key'
-            onInit={(evt, editor) => editorRef.current = editor}
-            initialValue={post?.text}
-            init={{
-            height: 500,
-            menubar: false,
-            plugins: [
-                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-            ],
-            toolbar: 'undo redo | blocks | ' +
-                'bold italic forecolor | alignleft aligncenter ' +
-                'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | help',
-            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            }}
-        />
-        <button onClick={log}>Log editor content</button>
-        </>
+        <div className='blogPostEditor'>
+            <form action="">
+                <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    value={post?.text || ""}
+                    init={{
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+                            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+                        ],
+                        toolbar: 'undo redo | blocks | ' +
+                            'bold italic forecolor | alignleft aligncenter ' +
+                            'alignright alignjustify | bullist numlist outdent indent | ' +
+                            'removeformat | help',
+                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                    }}
+                />
+                <button onClick={log}>Log editor content</button>
+            </form>
+        </div>
     );
 }
 
